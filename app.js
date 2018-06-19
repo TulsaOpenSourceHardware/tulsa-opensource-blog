f53245ebc16de29bdbbb546fdba4899476606fac
hasezoey <10911626+hasezoey@users.noreply.github.com>
2018-06-20 00:01:19 +0200

    Small Changes - app.js prettifying - adding some JSDOC - fix <h4></h2> Error - a bit more pretty - package.json:     Adding Repo     Adding author     rename to the the repo name

--- app.js
+++ app.js
@@ -1,52 +1,60 @@
-var data={
-		Title:"Tulsa Open Source",
-		Tagline:"",
-		mainStyle:"css/bootstrap.css"
-	}
+var data = {
+    Title: "Tulsa Open Source",
+    Tagline: "",
+    mainStyle: "css/bootstrap.css",
+    post: {},
+    posts: []
+}
 
-var express = require('express')
- , cons = require('consolidate')
- , mongodb = require('mongodb')
- , ObjectId = mongodb.ObjectID
- , app = express();
- 
-var DBSERVER = process.env.DBSERVER;
-var DBPASS = process.env.DBPASS;
-var DBUSER = process.env.DBUSER;
-var DBNAME = process.env.DBNAME;
-var DBPORT = process.env.DBPORT;
- 
-var dbserver = new mongodb.Server(DBSERVER,DBPORT, {});
-var db = new mongodb.Db(DBNAME, dbserver, {})
+const express = require('express'),
+    cons = require('consolidate'),
+    mongodb = require('mongodb'),
+    ObjectId = mongodb.ObjectID;
 
-db.open(function(err,res){
-  db.authenticate(DBUSER,DBPASS,function(err,res){
-          if(err){
-                  console.warn(err);
-          }else{
-                  console.log(res);
-          }
-  });
+var app = express();
+
+const config = {
+    dbServer: process.env.DBSERVER,
+    dbPort: process.env.DBPORT,
+    dbName: process.env.DBNAME,
+    dbUser: process.env.DBUSER,
+    dbPW: process.env.DBPASS,
+}
+
+var dbserver = new mongodb.Server(config.dbServer, config.dbPort, {});
+var db = new mongodb.Db(config.dbName, dbserver, {})
+
+db.open((err, res) => {
+    db.authenticate(DBUSER, DBPASS, (err, res) => {
+        if (err) {
+            console.warn(err);
+        } else console.log(res);
+    });
 }); 
 
-function stash(res,template,id){
-	if(id){
-		db.collection('posts').findOne({_id:new ObjectId(id)}, function(err,doc){
-			console.log(doc);
-			var toSend=data
-			toSend.post=doc;
-			res.render(template, toSend);
-		});
-	}else{
-		db.collection('posts').find({}).toArray(function(err, docs) {
-			docs.reverse()
-			var toSend=data
-			toSend.posts=docs
-			res.render('index', data);
-		});
-	}
+/**
+ * 
+ * @param {Response} res
+ * @param {string} template Path to Template File
+ * @param {number | string} id
+ */
+function stash(res, template, id) {
+    if (id) {
+        db.collection('posts').findOne({ _id: new ObjectId(id) }, (err, doc) => {
+            console.log(doc);
+            var toSend = Object.assign({}, data); //Copy
+            toSend.post = doc;
+            res.render(template, toSend);
+        });
+    } else {
+        db.collection('posts').find({}).toArray((err, docs) => {
+            docs.reverse();
+            var toSend = Object.assign({}, data); //Copy
+            toSend.posts = docs;
+            res.render('index', data);
+        });
+    }
 }
-		
  
 // assign the mustache engine to .html files
 app.engine('html', cons.mustache);
@@ -57,15 +65,10 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/templates');
  
 // test mustache
-app.get('/', function(req, res){
-  if(req.query.id){
-	  stash(res,'post.html',req.query.id)
-  }else{
-	stash(res,'index.html',null);
-  }
+app.get('/', (req, res) => {
+    if (req.query.id) {
+        stash(res, 'post.html', req.query.id);
+    } else stash(res, 'index.html', null);
 });
 
-
- 
-
-app.listen(process.env.PORT||3000);
+app.listen(process.env.PORT || 3000);
